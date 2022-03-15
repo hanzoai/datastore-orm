986bbe9148385c0f915c0b9dc0e2497e20e708b0
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2022-03-15 10:23:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 SETUP_INFO = dict(
     name = 'infi.clickhouse_orm',
-    version = '2.1.0.post15',
+    version = '2.1.0.post16',
     author = 'James Greenhill',
     author_email = 'fuziontech@gmail.com',
 
